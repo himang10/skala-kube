a0ab9611b72ea51f407081c03d310acf4d0350c4
himang10 <yongwooyi@gmail.com>
2026-09-15 17:42:11 +0900

    ingress update

--- 04.myfirst-api-server.old/02.configmap/createcm.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-USER_NAME=${STUDENT_NAME}
-NAMESPACE=${CLASS_NAME}
-
-kubectl create configmap ${USER_NAME}-myfirst-configmap \
-  --from-file=application-prod.yaml \
-  --namespace=${NAMESPACE}
-
--- 04.myfirst-api-server.old/07.mariadb/upgrade.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-helm upgrade mariadb-1 bitnami/mariadb \
-  -f custom-values.yaml
-
